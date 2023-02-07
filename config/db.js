9608da065dce09c764bcbdec2d77ff76d6d81cf6
sumon ghosh <sumong483@gmail.com>
2023-02-07 22:40:46 +0530

    fix

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 function connectDB() {
     // Database connection 🥳
-    mongoose.connect(process.env.MONGO_CONNECTION_URL, { useNewUrlParser: true, useCreateIndex:true, useUnifiedTopology: true, useFindAndModify : true });
+    mongoose.connect(process.env.MONGO_CONNECTION_URL, { useNewUrlParser: true,  useFindAndModify : true });
     const connection = mongoose.connection;
     connection.once('open', () => {
         console.log('Database connected');
