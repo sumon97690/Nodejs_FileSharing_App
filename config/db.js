42a6bb918942183cee79d50db86d1035947aeafc
sumon ghosh <sumong483@gmail.com>
2023-02-07 22:28:18 +0530

    fix

--- config/db.js
+++ config/db.js
@@ -5,10 +5,10 @@ function connectDB() {
     mongoose.connect(process.env.MONGO_CONNECTION_URL, { useNewUrlParser: true, useCreateIndex:true, useUnifiedTopology: true, useFindAndModify : true });
     const connection = mongoose.connection;
     connection.once('open', () => {
-        console.log('Database connected 🥳🥳🥳🥳');
-    }).catch(err => {
-        console.log('Connection failed ☹️☹️☹️☹️');
-    });
+        console.log('Database connected');
+    }) .on('error', function (err) {
+        console.log(err);
+      });
 }
 
 // mIAY0a6u1ByJsWWZ
